f879d10f9db329c9f484bb7a466f9bcebd0f0379
Nikolay Shishenkov <nshishenkov@gmail.com>
2020-04-13 08:36:19 -0400

    Update bbb-install.sh

--- bbb-install.sh
+++ bbb-install.sh
@@ -1,3 +1,6 @@
 apt-get update
 wget -qO- https://ubuntu.bigbluebutton.org/bbb-install.sh | bash -s -- -v xenial-220 -s re.gy -e email@gmail.com -g
 docker exec greenlight-v2 bundle exec rake user:create["Nick","email@gmail.com","password","admin"]
+cd /etc/nginx/conf.d/
+wget https://raw.githubusercontent.com/shishenkov/BBB-setup/master/force_ssl.conf
+reboot
